25d6cc07e624057c27acecf0d57d06ab0fec91e4
togeta83 <45890142+togeta83@users.noreply.github.com>
2018-12-19 22:35:30 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+/** Scroll to top button implementation in vanilla JavaScript (ES6 - ECMAScript 6) **/
+
+let intervalId = 0; // Needed to cancel the scrolling when we're at the top of the page
+const $scrollButton = document.querySelector('.scroll'); // Reference to our scroll button
+
+function scrollStep() {
+    // Check if we're at the top already. If so, stop scrolling by clearing the interval
+    if (window.pageYOffset === 0) {
+        clearInterval(intervalId);
+    }
+    window.scroll(0, window.pageYOffset - 50);
+}
+
+function scrollToTop() {
+    // Call the function scrollStep() every 16.66 millisecons
+    intervalId = setInterval(scrollStep, 16.66);
+}
+
+// When the DOM is loaded, this click handler is added to our scroll button
+$scrollButton.addEventListener('click', scrollToTop);
